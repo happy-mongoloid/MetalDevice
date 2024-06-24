ffae6725bc63b354c32a484bcf66297d4b001601
Nezhdanov Demian <nzhd.demian@gmail.com>
2024-06-24 01:23:27 -0300

    sa

---
+++ Sources/MetalDevice.xcframework/ios-arm64/MetalDevice.framework/Headers/MetalDevice.h
@@ -0,0 +1,18 @@
+//
+//  MetalDevice.h
+//  MetalDevice
+//
+//  Created by Demian Nezhdanov on 25/02/2024.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MetalDevice.
+FOUNDATION_EXPORT double MetalDeviceVersionNumber;
+
+//! Project version string for MetalDevice.
+FOUNDATION_EXPORT const unsigned char MetalDeviceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MetalDevice/PublicHeader.h>
+
+
